68e106edd8d8e34a509904d72becc48aaff9f87a
ruv-prog-so <rub3@pdx.edu>
2015-07-07 16:35:55 -0700

    - New layout for the user view with fragment. - Also inputted User and Organization test data to output to the form

--- app/src/main/java/com/bulletingroupblast/bulletingroupblast/HomeMainActivityFragment.java
+++ app/src/main/java/com/bulletingroupblast/bulletingroupblast/HomeMainActivityFragment.java
@@ -1,10 +1,14 @@
 package com.bulletingroupblast.bulletingroupblast;
 
+import com.bulletingroupblast.bulletingroupblast.Organization;
+import com.bulletingroupblast.bulletingroupblast.User;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
+import android.widget.TextView;
 
 
 /**
@@ -19,6 +23,15 @@ public class HomeMainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //lstOrganizations.list.add()
-        return inflater.inflate(R.layout.fragment_home_main, container, false);
+
+        User testUser = new User("ruben@gmail.com", "password", "Ruben", "Piatnitsky");
+        Organization testOrg = new Organization("Test Organization", "A description", testUser);
+
+        View rootView = inflater.inflate(R.layout.fragment_home_main, container, false);
+
+        final TextView textViewToChange = (TextView) rootView.findViewById(R.id.lblUserEmail);
+        textViewToChange.setText(testUser.getEmail());
+
+        return rootView;
     }
 }
