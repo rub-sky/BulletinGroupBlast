d3d9b51b55b9fe9ed747e9d62c5e460bdff685f6
ruv-prog-so <rub3@pdx.edu>
2015-07-07 16:32:10 -0700

    - Added Announcement, Event, Group, and Notification class files

---
+++ app/src/main/java/com/bulletingroupblast/bulletingroupblast/Announcement.java
@@ -0,0 +1,12 @@
+package com.bulletingroupblast.bulletingroupblast;
+
+/**
+ * Copyright © 2015 Ruben Piatnitsky
+ * This program is released under the "GNU license".
+ * Please see the file COPYING in this distribution for
+ * license terms.
+ * <p>
+ * Created by Ruben Piatnitsky on 7/6/15.
+ */
+public class Announcement {
+}
---
+++ app/src/main/java/com/bulletingroupblast/bulletingroupblast/Group.java
@@ -0,0 +1,13 @@
+package com.bulletingroupblast.bulletingroupblast;
+
+/**
+ * Copyright © 2015 Ruben Piatnitsky
+ * This program is released under the "GNU license".
+ * Please see the file COPYING in this distribution for
+ * license terms.
+ * <p>
+ * Created by Ruben Piatnitsky on 7/6/15.
+ */
+public class Group {
+    private int id;
+}
